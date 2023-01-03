d585487da581b82e1eda80996d5a331117c289b1
LudovicAns <ludovicanselin@gmail.com>
2023-01-03 10:24:30 +0100

    Add RyseInventory

--- src/main/java/fr/ludovicans/lanslib/LansLib.java
+++ src/main/java/fr/ludovicans/lanslib/LansLib.java
@@ -2,6 +2,13 @@ package fr.ludovicans.lanslib;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
+@SuppressWarnings("unused")
 public final class LansLib extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+        this.getServer().getLogger().log(Level.INFO, "LansLib loaded.");
+    }
 }
